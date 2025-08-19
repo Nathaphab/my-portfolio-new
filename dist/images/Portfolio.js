e37c4115c5e0a616ff12a30df24f68200ee67257
Nathaphab <nathaphab@example.com>
2025-08-20 01:48:55 +0700

    Deploy

---
+++ dist/images/Portfolio.js
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    id: 1,
+    title: "My Portfolio",
+    description: "Personal portfolio website built with React and TailwindCSS.",
+    image: "/images/portfolio.png", // รูปอยู่ใน public/images/
+    link: "https://instagram.fcnx4-1.fna.fbcdn.net/v/t1.15752-9/532324964_4371468496413946_6916459754074549758_n.png?_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_ohc=l0TYm7ZohnIQ7kNvwFHLyem&_nc_oc=AdlQjaL4bLnW9O_4QeC9JhvRLoKb_DVX1GnlbUiqwiZewo6bXlQXI8j7UxFR_fKZxRw&_nc_zt=23&_nc_ht=instagram.fcnx4-1.fna&oh=03_Q7cD3AE8OOSlNS3Z_JZ1ss1b3GaTQX0gwPW2ZtFOkO4a7FYk5A&oe=68CAC5D6"
+  }
+];
---
+++ dist/images/Weather App.js
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    id: 2,
+    title: "Weather App",
+    description: "Weather forecast app using OpenWeather API.",
+    image: "https://scontent.fcnx4-1.fna.fbcdn.net/v/t1.15752-9/533367934_747120204589074_4896499745075053235_n.png?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=ie0bZqryEbUQ7kNvwFIAcGA&_nc_oc=AdlrqYX5rYuG0oPwkYeJ7FvR8yLDgjaX4qkhIMJQC4zS3y_4-3-AvSsiuyvKdoLTCL4&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fcnx4-1.fna&oh=03_Q7cD3AE-mVJLOikmDbYC9gK5UH3MJTHRist6HoTttiaQOb0YiQ&oe=68CAD931", // ใช้ลิงก์ตรง
+    link: "https://scontent.fcnx4-1.fna.fbcdn.net/v/t1.15752-9/533367934_747120204589074_4896499745075053235_n.png?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=ie0bZqryEbUQ7kNvwFIAcGA&_nc_oc=AdlrqYX5rYuG0oPwkYeJ7FvR8yLDgjaX4qkhIMJQC4zS3y_4-3-AvSsiuyvKdoLTCL4&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fcnx4-1.fna&oh=03_Q7cD3AE-mVJLOikmDbYC9gK5UH3MJTHRist6HoTttiaQOb0YiQ&oe=68CAD931"
+  }
+];
